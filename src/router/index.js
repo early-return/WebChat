5f95122beb8130d20194bda18565925164840928
Zhiqing <i@zhiqing.inf>
2018-03-12 11:52:01 +0800

    Rename Session.vue to Conversation.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Session from '@/components/Session';
+import Conversation from '@/components/Conversation';
 import Chat from '@/components/Chat';
 import Group from '@/components/Group';
 import Status from '@/components/Status';
@@ -13,7 +13,7 @@ export default new Router({
     {
       path: '/',
       name: 'Hello',
-      component: Session,
+      component: Conversation,
     },
     {
       path: '/chat',
