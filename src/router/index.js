d19230a5e90624a088f42d1a4e0c98a4baa15254
Zhiqing <i@zhiqing.info>
2018-03-28 15:45:36 +0800

    添加路由认证代码

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import store from '@/store';
 import Converse from '@/components/converse/Converse';
 import Chat from '@/components/Chat';
 import Group from '@/components/Group';
@@ -10,7 +11,7 @@ import Login from '@/components/Login';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     {
@@ -27,22 +28,43 @@ export default new Router({
       path: '/chat',
       name: 'Chat',
       component: Chat,
+      meta: { requiresAuth: true },
     },
     {
       path: '/group',
       name: 'Group',
       component: Group,
+      meta: { requiresAuth: true },
     },
     {
       path: '/status',
       name: 'Status',
       component: Status,
+      meta: { requiresAuth: true },
     },
     {
       path: '/talk/:uid',
       name: 'Conserse',
       component: Converse,
       props: true,
+      meta: { requiresAuth: true },
     },
   ],
 });
+
+// 检查将要跳转的路由是否需要认证，如需要认证且当前没有认证即跳转到登陆界面
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!store.getters.self) {
+      next({
+        path: '/login',
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
