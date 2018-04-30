2a97983c819bedc7bab4dccf8ed23a09332d0f3e
zhiqing <i@zhiqing.info>
2018-04-30 19:20:42 +0800

    添加服务器端通过邮箱地址添加好友

--- server/handler/friend.js
+++ server/handler/friend.js
@@ -13,6 +13,15 @@ const processAddFriend = async (token, fromId, toId) => {
   return util.resp(true, '', null);
 };
 
+const processAddFriendWithEmail = async (token, fromId, toEmail) => {
+  const users = await db.findUser({ email: toEmail });
+  if (users.length < 1) {
+    throw new Error('未找到邮箱所对应的用户！');
+  }
+  const res = await processAddFriend(token, fromId, users[0]._id);
+  return res;
+};
+
 module.exports = {
   getFriends(req, res) {
     processGetFriends(req.params.token, req.params.uid)
@@ -21,6 +30,11 @@ module.exports = {
   },
 
   addFriend(req, res) {
+    if (req.body.toEmail) {
+      processAddFriendWithEmail(req.body.token, req.body.fromId, req.body.toEmail)
+        .then(data => res.json(data))
+        .catch(err => res.json(util.resp(false, err.message, err.toString())));
+    }
     processAddFriend(req.body.token, req.body.fromId, req.body.toId)
       .then(data => res.json(data))
       .catch(err => res.json(util.resp(false, err.message, err.toString())));
