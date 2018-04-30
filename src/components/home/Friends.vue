903d3d1e274a1b89d4970ba2433dee18573132b5
zhiqing <i@zhiqing.info>
2018-04-30 16:42:23 +0800

    完成好友界面

--- src/components/home/Friends.vue
+++ src/components/home/Friends.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>Menu</div>
+  <div>
+    <router-link class="friend" v-for="friend in friends" :key="friend._id" :to="`/talk/${friend._id}`">
+      <img class="avatar" :src="friend.avatar"><div class="name">{{ friend.name }}</div>
+    </router-link>
+  </div>
 </template>
 
 <script>
@@ -8,6 +12,33 @@ export default {
   data() {
     return {};
   },
+  computed: {
+    friends() {
+      return this.$store.state.friends;
+    },
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.friend {
+  display: flex;
+  align-items: center;
+  background: #FFF;
+  text-decoration: none;
+  color: #333;
+  padding: 10px 15px;
+
+  .avatar {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+  }
+
+  .name {
+    margin-left: 10px;
+  }
+}
+</style>
+
 
